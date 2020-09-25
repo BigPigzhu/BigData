0e0d739a32f5961881e9498ac5f5aa34e38c6045
qx <asdasd>
2020-09-25 16:40:16 +0800

    数据查询接口

--- spring/src/main/java/com/shujia/controller/StudentController.java
+++ spring/src/main/java/com/shujia/controller/StudentController.java
@@ -7,9 +7,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.*;
+import java.util.HashMap;
 
 @RestController
 public class StudentController {
+
+    /**
+     * 将已经查询过的数据保存到cache中
+     *
+     * 使用HashMap做缓存存在的问题
+     * 1、当数据量越来越来hashMap效率会降低
+     * 2、没办法清除没有用的缓存
+     *
+     *
+     */
+
+    HashMap<String,Student> cache = new HashMap<String,Student>();
+
 
     /**
      * 通过学号查询学生信息的接口
@@ -26,6 +40,20 @@ public class StudentController {
         }
 
 
+        /**
+         * 1、先查询缓存
+         *
+         * 如果缓存中有数据，直接返回，如果没有再查询数据库
+         */
+
+        Student student1 = cache.get(id);
+        if (student1!=null){
+
+            System.out.println("缓存中有数据，直接返回");
+            return student1;
+        }
+
+        System.out.println("缓存中没有数据查询数据库");
 
         /**
          * 连接数据库查询学生信息
@@ -51,6 +79,14 @@ public class StudentController {
 
 
                 Student student = new Student(id1,name,age,gender,clazz);
+
+                /**
+                 * 2、将查询回来的结果保存到缓存中
+                 *
+                 */
+
+                cache.put(id,student);
+
 
                 //spring boot 会自动将自定义类的对象转换成json格式的字符串返回
                 return student;

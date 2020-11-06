1ccb3671ea7b4aaf251c125354e6024c873c9a1c
qx <asd>
2020-11-06 09:51:31 +0800

    练习

---
+++ spark/src/main/java/com/shujia/spark/sql/Demo9Burk.scala
@@ -0,0 +1,90 @@
+package com.shujia.spark.sql
+
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.{Column, DataFrame, SparkSession}
+
+object Demo9Burk {
+  def main(args: Array[String]): Unit = {
+
+    val spark: SparkSession = SparkSession.builder()
+      .master("local")
+      .appName("brk")
+      .config("spark.sql.shuffle.partitions", 2)
+      .getOrCreate()
+
+    import spark.implicits._
+    import org.apache.spark.sql.functions._
+
+
+    val burk: DataFrame = spark.read
+      .format("csv")
+      .option("sep", ",")
+      .schema("burk STRING , year STRING , tsl01 DOUBLE ,tsl02 DOUBLE ,tsl03 DOUBLE ,tsl04 DOUBLE ,tsl05 DOUBLE ,tsl06 DOUBLE ,tsl07 DOUBLE ,tsl08 DOUBLE ,tsl09 DOUBLE ,tsl10 DOUBLE ,tsl11 DOUBLE ,tsl12 DOUBLE ")
+      .load("spark/data/burks.txt")
+
+
+    //行数
+    //    burk.show(1000)
+
+    //完全显示一行数据
+    //    burk.show(truncate = true)
+
+
+    /**
+      *
+      * 1、统计每个公司每年按月累计收入
+      *
+      * 输出结果
+      * 公司代码,年度,月份,当月收入,累计收入
+      *
+      */
+
+    burk.createOrReplaceTempView("burk")
+
+
+    /**
+      * sql
+      *
+      */
+
+
+    spark.sql(
+      """
+        |
+        |select burk,year,month,pic, sum(pic)  over(partition by burk,year  order by month) as sum_pic from
+        |(
+        |select burk,year,month,pic from burk
+        |LATERAL VIEW explode(map('01',tsl01,'02',tsl02,'03',tsl03,'04',tsl04,'05',tsl05,'06',tsl06,'07',tsl07,'08',tsl08,'09',tsl09,'10',tsl10,'11',tsl11,'12',tsl12)) t  as month,pic
+        |)
+        |
+      """.stripMargin).show(1000)
+
+
+    /**
+      * DSL
+      *
+      */
+
+    val map1: Column = map(
+      expr("01"), $"tsl01",
+      expr("02"), $"tsl02",
+      expr("03"), $"tsl03",
+      expr("04"), $"tsl04",
+      expr("05"), $"tsl05",
+      expr("06"), $"tsl06",
+      expr("07"), $"tsl07",
+      expr("08"), $"tsl08",
+      expr("09"), $"tsl09",
+      expr("10"), $"tsl10",
+      expr("11"), $"tsl11",
+      expr("12"), $"tsl12"
+    )
+
+
+    burk
+      .select($"burk", $"year", explode(map1) as Array("month", "pic")) //列转行
+      .select($"burk", $"year", $"month", $"pic", sum($"pic") over Window.partitionBy($"burk", $"year").orderBy($"month") as "sum_pic")
+      .show()
+
+  }
+}
